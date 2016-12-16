b919c6eecbdb74024bed6630c5bfc6d79e99c163
jim <liu1084@163.com>
2016-12-16 17:18:13 +0800

    update

---
+++ src/app/Login.vue
@@ -0,0 +1,30 @@
+<template>
+	<section class="neu-main">
+		<div class="neu-main-container">
+			<ul class="neu-login">
+				<li class="title">
+					<h1 class="logo-name">UTF v2</h1>
+				</li>
+				<li class="neu-login-form">
+					<form id="logonform" method="post" action="">
+						<div class="neu-form-group">
+							<input id="username" name="username" type="text" class="neu-form-control" placeholder="Username" />
+						</div>
+						<div class="neu-form-group">
+							<input id="password" name="password" type="password" class="neu-form-control" placeholder="Password" />
+						</div>
+						<button type="button" id="login-btn" class="neu-btn neu-btn-primary full-width">Login</button>
+					</form>
+				</li>
+				<li class="copyright">All right reserved.&copy; Neusoft Co Ltd.</li>
+				<li class="copyright">2015 - 2016</li>
+			</ul>
+		</div>
+	</section>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
